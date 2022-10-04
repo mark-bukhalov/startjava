939ce86841d03b3f25ec8858c607fe3e3a760878
Бухалов Марк Васильевич <mbuhalov@msc.sigma-it.local>
2022-10-04 20:33:34 +0300

    Lesson_2 task_1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "Мужской";
+    String name = "Иван";
+    int height = 170;
+    int weight = 77;
+    int age = 33;
+
+    void go() {
+        System.out.println("Иду");
+    }
+
+    void sit() {
+        System.out.println("Сижу");
+    }
+
+    void run() {
+        System.out.println("Бегу");
+    }
+
+    void talk() {
+        System.out.println("Привет!");
+    }
+
+    void learnJava() {
+        System.out.println("Учу Java");
+    }
+}
